d2f20adca3b5aa146d0a539819d9b69c5f9c7f95
yhc5091 <yhc5091@gmail.com>
2024-05-18 17:19:22 +0900

    langchain RAG + chatMemory

--- langwrapper.py
+++ langwrapper.py
@@ -1,60 +1,73 @@
 import dotenv
 import os
+import hashlib
+from operator import itemgetter
 
+# OpenAI
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationChain
+
+# Chain
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain.schema.runnable import RunnablePassthrough
 from langchain.memory import ConversationSummaryBufferMemory
+
+# Embedding, RAG
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import chroma
-from langchain.vectorstores import Chroma
-from langchain_core.callbacks.base import BaseCallbackHandler
-from langchain.schema.runnable import RunnablePassthrough
-from langchain import hub
-from langchain.chains import RetrievalQA
+
 
 # load .env
 dotenv.load_dotenv()
 
-params = {
-    "temperature": 1,           # 생성된 텍스트의 다양성 조정
-    "max_tokens": 100,          # 생성할 최대 토큰 수
-}
+class WrapModel:
+    llm = None
+    conv = None
 
-kwargs = {
-    "frequency_penalty": 0.5,   # 이미 등장한 단어의 재등장 확률
-    "presence_penalty": 0.5,    # 새로운 단어의 도입을 장려
-    "stop": ["\n"]              # 정지 시퀀스 설정
+    params = {
+        "temperature": 0.5,           # 생성된 텍스트의 다양성 조정
+        "max_tokens": 1000,          # 생성할 최대 토큰 수
+    }
 
-}
+    kwargs = {
+        "frequency_penalty": 0.5,   # 이미 등장한 단어의 재등장 확률
+        "presence_penalty": 0.5,    # 새로운 단어의 도입을 장려
+        "stop": ["\n"]              # 정지 시퀀스 설정
+
+    }
+
+    def __init__(self):
+        self.llm = ChatOpenAI(
+            model="gpt-3.5-turbo-0125", 
+            **self.params, 
+            model_kwargs = self.kwargs,
+            )
+        self.conv = ConversationSummaryBufferMemory(
+            memory_key="chat_memory",
+            return_messages=True,
+            llm = self.llm, 
+            max_token_limit = 4000,
+            )
+    
 
 def InitModel():
-    llm = ChatOpenAI(
-        model="gpt-3.5-turbo-0125", 
-        **params, 
-        model_kwargs = kwargs,
-        )
-    conv = ConversationSummaryBufferMemory(
-        llm=llm, 
-        max_token_limit=100,
-        )
-    conversation = ConversationChain(
-        llm=llm, 
-        memory = conv,
-        verbose=True,
-    )
-    return conversation
+    return WrapModel()
     
 
 def ReadFile(uploaded_file):
-    temp_file = "./temp.pdf"
-    with open(temp_file, "wb") as file:
-        file.write(uploaded_file.getvalue())
-        file_name = uploaded_file.name
+    data=uploaded_file.getvalue()
+    hash=hashlib.sha256(data).hexdigest()
+
+    temp_file = "./" + hash + ".pdf"
+
+    if os.path.isfile(temp_file) is False:
+        with open(temp_file, "wb") as file:
+            file.write(data)
     
     loader = PyPDFLoader(temp_file)
     documents = loader.load_and_split()
@@ -63,52 +76,55 @@ def ReadFile(uploaded_file):
     texts = text_splitter.split_documents(documents)
     
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-    db = Chroma.from_documents(documents=texts, embedding=embeddings)
+    cache_dir = LocalFileStore("./.cache/practice/")
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    db = FAISS.from_documents(texts, cached_embeddings)
     retriever = db.as_retriever()
     return retriever
 
 def Prompt(model, prompt, uploadedFile):
+    retriever = None
     if uploadedFile is not None:
         retriever = ReadFile(uploadedFile)
-        promptTemplate = ChatPromptTemplate.from_messages(
-            [
-                (
-                    "system",
-                    """
-                    You are a helpful assistant. 
-                    Answer questions using only the following context. 
-                    If you don't know the answer just say you don't know, don't make it up:
-                    \n\n
-                    {context}",
-                    """
-                ),
-                ("human", "{question}"),
-            ]
+        
+    promptTemplate = ChatPromptTemplate.from_messages([
+        ("system", 
+         """
+         You are a helpful assistant, conversation friend.
+         Answer questions using only the following context. 
+         If you don't know the answer just say you don't know, don't make it up:
+         \n\n
+         {context}",
+         """
+         ),
+        MessagesPlaceholder(variable_name="chat_memory"),
+        ("human", "{question}"),
+    ])
+
+    if retriever is None:
+        context = RunnablePassthrough.assign(
+            context=itemgetter("question")
+        )
+    else:
+        context = RunnablePassthrough.assign(
+            context=itemgetter("question") | retriever,
         )
 
-        llm = ChatOpenAI(
-            model="gpt-3.5-turbo-0125", 
-            **params, 
-            model_kwargs = kwargs,
-        )
-        chain = (
-            {
-                "context": retriever,
-                "question": RunnablePassthrough(),
-            }
-            | promptTemplate
-            | llm
-            | StrOutputParser()
-        )
-        output = chain.invoke(prompt)
-        # output = model.invoke({"context": retriever, "input":prompt})
-        # p = hub.pull("rlm/rag-prompt")
-        # qa_chain = RetrievalQA.from_chain_type(
-        #     model,
-        #     retriever=retriever,
-        #     chain_type_kwargs={"prompt": p}
-        # )
-        # output = qa_chain(query=prompt)
-    else:
-        output = model.predict(input=prompt)
-    return output
+    chain = (
+        context |
+        RunnablePassthrough.assign(chat_memory=lambda input : model.conv.load_memory_variables({})["chat_memory"])
+        | promptTemplate
+        | model.llm
+    )
+    print(model.conv.load_memory_variables({}))
+    output = invoke_chain(chain, model.conv, prompt)
+    return output.content
+
+def invoke_chain(chain, memory, question):
+    result = chain.invoke({"question": question})
+    # memory에 대화 내용을 저장한다.
+    memory.save_context(
+        {"input": question},
+        {"output": result.content},
+    )
+    return result
